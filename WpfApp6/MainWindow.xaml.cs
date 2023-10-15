72d08b5cdd7805a9544183ee97767a87a459ec2d
Вадим <@DESKTOP-SBB1HEM>
2023-10-15 23:17:32 +0300

    практос 6

--- WpfApp6/MainWindow.xaml.cs
+++ WpfApp6/MainWindow.xaml.cs
@@ -24,5 +24,52 @@ namespace WpfApp6
         {
             InitializeComponent();
         }
+
+        private void btnExit_click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnInfo_click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Выполнил лучший разработчик: Девяткин Вадим Евгеньевич\r\nПрактическая №5\r\nСоздать класс Pair (пара четных чисел). Создать необходимые методы и свойства.Определить метод перемножения пар(а, b) * (с, d) = (а * c, b * d).Создать перегруженный метод для удвоения пары чисел.");
+        }
+
+        private void btnDa_click(object sender, RoutedEventArgs e)
+        {
+            string result1 = tbA.Text.Replace(" ", "");
+            string result2 = tbB.Text.Replace(" ", "");
+            if (int.TryParse(result1, out int A) && int.TryParse(result2, out int B))
+            {
+                string userInput1 = tbA.Text;
+                string userInput2 = tbB.Text;
+                string[] numbersArray1 = userInput1.Split(' ');
+                string[] numbersArray2 = userInput2.Split(' ');
+
+                if (numbersArray1.Length == 2 && numbersArray2.Length == 2)
+                {
+                    int.TryParse(numbersArray1[0], out int a);
+                    int.TryParse(numbersArray1[1], out int b);
+                    int.TryParse(numbersArray2[0], out int c);
+                    int.TryParse(numbersArray2[1], out int d);
+
+                    Pair pair1 = new Pair(a, b);
+                    Pair pair2 = new Pair(c, d);
+                    Pair multiplicationResult = pair1 * pair2;
+                    tbRes1.Text = ($"Перемножениe пар {a}*{c}, {b}*{d} = {multiplicationResult.First}, {multiplicationResult.Second}");
+
+                    Pair incrementResult = ++pair1;
+                    tbRes2.Text = ($"Результат инкремента ({incrementResult.First}, {incrementResult.Second})");
+                }
+                else
+                {
+                    MessageBox.Show("Введите по 2 числа через пробел например: 1 2");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Введите числа");
+            }
+        }
     }
 }
---
+++ WpfApp6/Pair.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp6
+{
+    public class Pair
+    {
+        public int First { get; set; }
+        public int Second { get; set; }
+
+        public Pair(int first, int second)
+        {
+            First = first;
+            Second = second;
+        }
+
+        public static Pair operator *(Pair a, Pair b)
+        {
+            int resultFirst = a.First * b.First;
+            int resultSecond = a.Second * b.Second;
+            return new Pair(resultFirst, resultSecond);
+        }
+
+        public static Pair operator ++(Pair p)
+        {
+            int doubledFirst = p.First * 2;
+            int doubledSecond = p.Second * 2;
+            return new Pair(doubledFirst, doubledSecond);
+        }
+    }
+}
