2807a7377d6d57dee4e169895d0483bb30cc1a58
JPKakaAG <Gjopka@yandex.ru>
2023-10-16 11:13:38 +0300

    парктическая 6

--- WpfApp6/Pair.cs
+++ WpfApp6/Pair.cs
@@ -11,12 +11,23 @@ namespace WpfApp6
         public int First { get; set; }
         public int Second { get; set; }
 
+        /// <summary>
+        /// Конструктор, принимающий значения для обоих чисел в паре
+        /// </summary>
+        /// <param name="first"> первое число из пары</param>
+        /// <param name="second"> второе число из пары</param>
         public Pair(int first, int second)
         {
             First = first;
             Second = second;
         }
 
+        /// <summary>
+        /// Перегрузка оператора умножения для пар
+        /// </summary>
+        /// <param name="a">первое число из второй пары</param>
+        /// <param name="b">второе число из второй пары</param>
+        /// <returns>возвращает результат перемножения</returns>
         public static Pair operator *(Pair a, Pair b)
         {
             int resultFirst = a.First * b.First;
@@ -24,6 +35,11 @@ namespace WpfApp6
             return new Pair(resultFirst, resultSecond);
         }
 
+        /// <summary>
+        /// Перегрузка оператора инкремента для удвоения пары чисел
+        /// </summary>
+        /// <param name="p">объект</param>
+        /// <returns>возвращает результат удвоения пары чисел</returns>
         public static Pair operator ++(Pair p)
         {
             int doubledFirst = p.First * 2;
